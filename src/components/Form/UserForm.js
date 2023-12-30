f309057a17ce1f34b92625f9cc94a5e5f8738119
H-S-RATHI <goluu.000111@gmail.com>
2023-12-30 19:42:34 +0530

    folder structured

---
+++ src/components/Form/UserForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserForm = () => {
+  return <div>userForm</div>;
+};
+
+export default UserForm;
---
+++ src/pages/home.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import UserForm from "../components/Form/UserForm";
+import BookTable from "../components/Table/BookTable";
+import { fetchData, saveBook } from "../services/api";
+
+const Home = () => {
+  const [booksData, setBooksData] = useState([]);
+
+  useEffect(() => {
+    displayBooks();
+  }, []);
+
+  const displayBooks = async () => {
+    try {
+      const data = await fetchData(); // Function to fetch book data from API
+      setBooksData(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const addNewBook = async (newBook) => {
+    try {
+      await saveBook(newBook); // Function to save a new book to the API
+      displayBooks(); // Refresh book list after adding a new book
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Book Donate</h1>
+      <UserForm addNewBook={addNewBook} />
+      <BookTable books={booksData} displayBooks={displayBooks} />
+    </div>
+  );
+};
+
+export default Home;
