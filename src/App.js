1341eeafd19744717ac344bea8e6698ab7355634
H-S-RATHI <goluu.000111@gmail.com>
2023-12-31 21:15:32 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css"; // Import your CSS file here
+import {
+  getUserBooks,
+  saveNewBook,
+  updateBook,
+  deleteBook,
+} from "./services/api"; // Import your API functions here
+import UserForm from "../components/Form/UserForm";
+import BookTable from "../components/Table/BookTable";
 
 function App() {
+  const [booksData, setBooksData] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = () => {
+    getUserBooks()
+      .then((data) => setBooksData(data))
+      .catch((error) => console.error("Error fetching books:", error));
+  };
+
+  const handleSaveBook = (book) => {
+    saveNewBook(book)
+      .then(() => fetchBooks())
+      .catch((error) => console.error("Error saving book:", error));
+  };
+
+  const handleUpdateBook = (bookId, updatedBook) => {
+    updateBook(bookId, updatedBook)
+      .then(() => fetchBooks())
+      .catch((error) => console.error("Error updating book:", error));
+  };
+
+  const handleDeleteBook = (bookId) => {
+    deleteBook(bookId)
+      .then(() => fetchBooks())
+      .catch((error) => console.error("Error deleting book:", error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Book Donate</h1>
+      <UserForm onSave={handleSaveBook} />
+      <BookTable
+        books={booksData}
+        onUpdate={handleUpdateBook}
+        onDelete={handleDeleteBook}
+      />
     </div>
   );
 }
